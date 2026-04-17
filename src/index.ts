e4179317bec08c6ae6f011faebe05d5e26280e3d
Klaas Schoute <klaas_schoute@hotmail.com>
2026-04-17 18:33:26 +0200

    fix: enable console logging for debugging in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import "./editors/netlink-access-code-card-editor";
 import "./editors/netlink-status-card-editor";
 import "./cards/netlink-access-code-card";
@@ -8,5 +9,5 @@ declare const __VERSION__: string;
 console.info(
   `%c NETLINK-HA-CARDS %c v${__VERSION__} `,
   "color: white; background: #0ea5e9; font-weight: 700;",
-  "color: #0ea5e9; background: white; font-weight: 700;",
+  "color: #0ea5e9; background: white; font-weight: 700;"
 );
